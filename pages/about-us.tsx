dd776f4fde753200e90afbe41ee53223edc5646e
KJ Sudarshan <kjsudi@gmail.com>
2022-09-29 10:55:12 +0530

    add: new pages and export command

---
+++ pages/about-us.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const AboutUs: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Deep Armor | About Us</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      About Us
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ pages/solutions.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Solutions: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Deep Armor | Solutions</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Solutions
+    </div>
+  );
+};
+
+export default Solutions;
